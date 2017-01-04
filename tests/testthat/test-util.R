c00266646ec4c416598ffe947c6d0fb73a85ca5e
Frank Portman <fportman@uber.com>
2017-01-03 17:39:59 -0800

    test startup

--- tests/testthat/test-util.R
+++ tests/testthat/test-util.R
@@ -5,4 +5,6 @@ test_that("Success", {
   
   expect_silent(ppareto(5, 4, 3))
   
+  expect_message({detach(package:bayesAB); library(bayesAB)}, "See ?bayesTest for the guts of the bayesAB package and/or ?plotDistributions for help on choosing priors!", fixed = TRUE)
+  
 })
