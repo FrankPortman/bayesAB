2ad750d484d8b658ed1f097d31ad523f26925c8f
Frank Portman <fportman@uber.com>
2017-01-03 17:01:43 -0800

    reduce lines

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,3 @@
 .onAttach <- function(...) {
-  if (!interactive()) return()
-  packageStartupMessage("See ?bayesTest for the guts of the bayesAB package and/or ?plotDistributions for help on choosing priors!")
+  if (!interactive()) return() else packageStartupMessage("See ?bayesTest for the guts of the bayesAB package and/or ?plotDistributions for help on choosing priors!")
 }
