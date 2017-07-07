7fa6186f3ac5b30e4faeb62cdfce38643f179e07
Frank <frank1214@gmail.com>
2017-07-07 12:18:01 -0700

    Remove unnecessary listConcat function

--- R/util.R
+++ R/util.R
@@ -18,17 +18,6 @@ getPostError <- function(A_samples, B_samples) {
   coalesce(mean(B_samples > A_samples) * mean(B_samples[B_samples > A_samples]))
 }
 
-listConcat <- function(l1, l2) {
-
-  out <- vector(mode = "list", length = length(l1) + length(l2))
-
-  for(i in 1:length(l1)) out[[i]] <- l1[[i]]
-  for(i in 1:length(l2)) out[[i + length(l1)]] <- l2[[i]]
-
-  out
-
-}
-
 listOr <- function(e) if(is.list(e)) e else list(e)
 
 isClosed <- function(distribution) grepl("C", distribution)
