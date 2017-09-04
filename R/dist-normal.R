2426dfaf507c938c4d0634062c557dc863ba0c10
Frank <frank1214@gmail.com>
2017-09-04 13:00:06 -0700

    hotfix normal

--- R/dist-normal.R
+++ R/dist-normal.R
@@ -16,14 +16,13 @@ drawMusAndSigmas <- function(data,
 
   vN_times_s_sqN <- v0 * s_sq0 + ssd + (N * k0 * (m0 - the_mean) ^ 2) / kN
 
-
   alpha <- vN / 2
   beta <- vN_times_s_sqN / 2
 
   sig_sq_samples <- (1 / rgamma(n_samples, alpha, beta))
 
   mean_norm <- mN
-  var_norm <- sqrt(sig_sq_samples / kN)
+  var_norm <- sqrt(sig_sq_samples) / kN
   mu_samples <- rnorm(n_samples, mean_norm, var_norm)
 
   return(list(mu_samples = mu_samples, sig_sq_samples = sig_sq_samples))
