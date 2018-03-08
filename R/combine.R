ed8d83b1fec4d4c998e0dfb405e341f329b8dc37
ThibautJoncquez <thibaut.joncquez@gmail.com>
2018-03-08 14:52:19 -0800

    Override the combine function to take standard input

--- R/combine.R
+++ R/combine.R
@@ -47,39 +47,65 @@
 #' summary(AB3)
 #' plot(AB3)
 #' @export
-combine <- function(bT1, bT2, f = `+`, params, newName) {
-  if(
-    any(
-      isClosed(bT1$inputs$distribution),
-      isClosed(bT2$inputs$distribution)
-    )
-  ) stop("Can't combine a 'closed' bayesTest.")
+# combine <- function(bT1, bT2, f = `+`, params, newName) {
+#   if(
+#     any(
+#       isClosed(bT1$inputs$distribution),
+#       isClosed(bT2$inputs$distribution)
+#     )
+#   ) stop("Can't combine a 'closed' bayesTest.")
+# 
+#   if(missing(params)) {
+#     params <- c(NA, NA)
+#     params[1] <- names(bT1$posteriors)[1]
+#     params[2] <- names(bT2$posteriors)[1]
+#   }
+# 
+#   if(missing(newName)) {
+#     input <- paste0(params, collapse = ", ")
+#     input <- paste0('(', input, ')')
+#     newName <- paste0(toString(substitute(f)), input)
+#   }
+# 
+#   if(length(params) != 2) stop('You must specify only (2) params - one for the first test and one for the second test.')
+# 
+#   if(!((params[1] %in% names(bT1$posteriors)) & (params[2] %in% names(bT2$posteriors)))) {
+#     stop("You have specified a `param` name that doesn't exist in the posterior of the first test and/or the second test.")
+#   }
+# 
+#   if(bT1$inputs$n_samples != bT2$inputs$n_samples) warning("n_samples not equal. Make sure `f` handles recycling appropriately.")
+# 
+#   A1 <- bT1$posteriors[[params[1]]]$A; A2 <- bT2$posteriors[[params[2]]]$A
+#   B1 <- bT1$posteriors[[params[1]]]$B; B2 <- bT2$posteriors[[params[2]]]$B
+# 
+#   result <- list()
+# 
+#   result$inputs <- list(
+#     A_data = c(bT1$inputs$A_data, bT2$inputs$A_data),
+#     B_data = c(bT1$inputs$B_data, bT2$inputs$B_data),
+#     priors = 'Combined distributions have no priors. Inspect each element separately for details.',
+#     n_samples = max(bT1$inputs$n_samples, bT2$inputs$n_samples),
+#     distribution = 'combined'
+#   )
+# 
+#   result$prior <- NULL
+#   result$posteriors[[newName]] <- list(A = f(A1, A2), B = f(B1, B2))
+# 
+#   class(result) <- c('bayesTest')
+# 
+#   return(result)
+# 
+# }
 
-  if(missing(params)) {
-    params <- c(NA, NA)
-    params[1] <- names(bT1$posteriors)[1]
-    params[2] <- names(bT2$posteriors)[1]
-  }
+## combine Combine two bayesAB objects given a binary function
 
-  if(missing(newName)) {
-    input <- paste0(params, collapse = ", ")
-    input <- paste0('(', input, ')')
-    newName <- paste0(toString(substitute(f)), input)
-  }
-
-  if(length(params) != 2) stop('You must specify only (2) params - one for the first test and one for the second test.')
-
-  if(!((params[1] %in% names(bT1$posteriors)) & (params[2] %in% names(bT2$posteriors)))) {
-    stop("You have specified a `param` name that doesn't exist in the posterior of the first test and/or the second test.")
-  }
-
-  if(bT1$inputs$n_samples != bT2$inputs$n_samples) warning("n_samples not equal. Make sure `f` handles recycling appropriately.")
-
-  A1 <- bT1$posteriors[[params[1]]]$A; A2 <- bT2$posteriors[[params[2]]]$A
-  B1 <- bT1$posteriors[[params[1]]]$B; B2 <- bT2$posteriors[[params[2]]]$B
-
+combine <- function(bT1, bT2){
+  A1 <- bT1$posteriors$Probability$A 
+  A2 <- bT2$posteriors$Mean$A
+  B1 <- bT1$posteriors$Probability$B
+  B2 <- bT2$posteriors$Mean$B
+  
   result <- list()
-
   result$inputs <- list(
     A_data = c(bT1$inputs$A_data, bT2$inputs$A_data),
     B_data = c(bT1$inputs$B_data, bT2$inputs$B_data),
@@ -87,14 +113,12 @@ combine <- function(bT1, bT2, f = `+`, params, newName) {
     n_samples = max(bT1$inputs$n_samples, bT2$inputs$n_samples),
     distribution = 'combined'
   )
-
+  
   result$prior <- NULL
-  result$posteriors[[newName]] <- list(A = f(A1, A2), B = f(B1, B2))
-
+  result$posteriors[['Probability']] <- list(A = A1*A2, B = B1*B2)
+  
   class(result) <- c('bayesTest')
-
   return(result)
-
 }
 
 #' @export
